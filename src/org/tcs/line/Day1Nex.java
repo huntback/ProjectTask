2e4a5c657f94ca8190adae27004f8c39a9a3c9d4
huntback <wix.sqiz@gmail.com>
2018-07-25 12:18:44 +0530

    added function add(double,double)

--- src/org/tcs/line/Day1Nex.java
+++ src/org/tcs/line/Day1Nex.java
@@ -2,14 +2,21 @@ package org.tcs.line;
 
 public class Day1Nex {
 public static void main(String[] args) {
-	System.out.println("dhinesh");\
+	System.out.println("dhinesh");
 	add(5,6);
+	add(5.5,6.5);
 }
 
 
 public static void add(int i,int j)
 {
 	int c = i+j;
+	System.out.println(c);
+}
+
+public static void add(double i,double j)
+{
+	double c = i+j;
 	System.out.println(c);
 }
 }
