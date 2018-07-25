5aaebcc5e62015293fcd17f150f5660b5aa5cbed
huntback <wix.sqiz@gmail.com>
2018-07-25 12:07:57 +0530

    added add(string,string)

--- src/org/tcs/line/Day1Nex.java
+++ src/org/tcs/line/Day1Nex.java
@@ -3,5 +3,14 @@ package org.tcs.line;
 public class Day1Nex {
 public static void main(String[] args) {
 	System.out.println("dhinesh");
+	add("Siddharthan", "Prabakaran");
 }
+
+public static void add(String a,String b)
+{
+	String c = a+b;
+	System.out.println(c);
+}
+
+
 }
